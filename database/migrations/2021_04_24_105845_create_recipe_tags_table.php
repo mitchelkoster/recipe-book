8e47c4efc749d9b812023540858ece0d2059c27e
Mitchel Koster <mitchelkoster@gmail.com>
2021-04-24 13:03:39 +0200

    Added RecipeTag model

--- database/migrations/2021_04_24_105845_create_recipe_tags_table.php
+++ database/migrations/2021_04_24_105845_create_recipe_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class CreateRecipeTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('recipe_tags', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('user_id')->onDelete('cascade');
+            $table->foreignId('tag_id')->onDelete('cascade');
         });
     }
 
@@ -26,6 +27,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('recipe_tags');
     }
 }
