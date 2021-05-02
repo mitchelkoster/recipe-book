89fc8fff1dcf8259cc00b9e7f3afe518d872eb1c
Mitchel Koster <mitchelkoster@gmail.com>
2021-05-02 12:20:54 +0200

    Created base API for products

---
+++ app/Http/Controllers/Api/IngredientController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Ingredient;
+use Illuminate\Http\Request;
+
+class IngredientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Ingredient::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        abort(404);
+    }
+}
