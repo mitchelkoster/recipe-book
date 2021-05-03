c6fdfdb027a8df1f22c3963d0aee92593d02a217
Mitchel Koster <mitchelkoster@gmail.com>
2021-05-03 23:54:17 +0200

    ...

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
 require('./bootstrap');
-
-// Import Vue
-import Vue from 'vue';
-import App from './components/recipes-create';
-
-const app = new Vue({
-    el: '#app',
-    components: { App }
-});
-
+require('./vue');
 require('alpinejs');
---
+++ resources/js/vue.js
@@ -0,0 +1,8 @@
+// Import Vue
+import Vue from 'vue';
+import App from './components/recipes-create';
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});
