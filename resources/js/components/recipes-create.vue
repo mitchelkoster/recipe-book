69ea731641832e10255ba89d5f24073a98c52be0
Mitchel Koster <mitchelkoster@gmail.com>
2021-05-09 12:39:44 +0200

    Updated form style & added compiled files to gitignore

--- resources/js/components/recipes-create.vue
+++ resources/js/components/recipes-create.vue
@@ -29,16 +29,12 @@
             <fieldset class="border px-4">
                 <legend class="text-2xl text-green-600 px-8">Ingredients</legend>
 
-                <!-- Ingredient name -->
-                <div  class="mt-4">
-                    <label class="block rounded">
-                        <span class="block font-semibold text-md text-gray-700">Ingredient</span>
-
-                        <input id="ingredient"
-                               class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
-                               type="text" list="ingredients" name="ingredient" placeholder="Search for ingredients..." required
-                               autofocus/>
-                    </label>
+                <div class="my-4 flex flex-col md:flex-row justify-center items-stretch">
+                    <!-- Ingredient name -->
+                    <input id="ingredient"
+                           class="mb-4 md:mb-0 md:w-3/6 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           type="text" list="ingredients" name="ingredient" placeholder="Search ingredient" required
+                           autofocus/>
 
                     <datalist id="ingredients">
                         <option value="Carrot"/>
@@ -47,30 +43,29 @@
                         <option value="Tumeric"/>
                         <option value="Lentils"/>
                     </datalist>
+
+                    <!-- Ingredient quantity -->
+                    <input id="qty"
+                       class="mb-4 md:mb-0 md:mx-2 md:w-1/6 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                       type="number" name="qty" value="1" step="1" required
+                       autofocus/>
+
+                    <!-- Measurement -->
+                    <select
+                        class="md:w-2/6 form-select rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                        <option>Grams (Metric)</option>
+                        <option>Cups (Imperial)</option>
+                    </select>
                 </div>
 
-                <!-- Ingredient quantity -->
-                <div class="mt-4">
-                    <label class="block rounded">
-                        <span class="block font-semibold text-md text-gray-700">Quantity</span>
-
-                        <input id="qty"
-                               class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
-                               type="number" name="qty" value="1" step="1" required
-                               autofocus/>
-                    </label>
-                </div>
-
-                <!-- Measurement -->
-                <div class="mt-4 mb-8">
-                    <label class="block rounded">
-                        <span class="block font-semibold text-md text-gray-700">Measurement</span>
-                        <select
-                            class="form-select block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full">
-                            <option>Grams (Metric)</option>
-                            <option>Cups (Imperial)</option>
-                        </select>
-                    </label>
+                <!-- Add additional ingredient -->
+                <div class="flex items-center justify-end my-4">
+                    <a class="inline-flex items-center text-green-600 hover:text-green-500" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                        </svg>
+                        Add
+                    </a>
                 </div>
             </fieldset>
         </div>
@@ -79,27 +74,30 @@
         <div class="mt-4">
             <fieldset class="border px-4">
                 <legend class="text-2xl text-green-600 px-8">Steps</legend>
+
                 <!-- Step title -->
                 <div class="mt-4">
-                    <label class="block rounded">
-                        <span class="block font-semibold text-md text-gray-700">Title</span>
-
-                        <input id="step-title"
-                               class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
-                               type="text" name="title" placeholder="Prepping the veggies" required
-                               autofocus/>
-                    </label>
+                    <input id="step-title"
+                           class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
+                           type="text" name="title" placeholder="Vegetable prep (optional)" required
+                           autofocus/>
                 </div>
 
                 <!-- Step description -->
                 <div class="mt-4 mb-8">
-                    <label class="block rounded">
-                        <span class="block font-semibold text-md text-gray-700">Description</span>
+                    <textarea id="step-description" rows="5"
+                          class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
+                          type="email" name="description" placeholder="Dice up one carrot in large chunks, quarter a pepper, etc." required></textarea>
+                </div>
 
-                        <textarea id="step-description" rows="5"
-                                  class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50' block mt-1 w-full"
-                                  type="email" name="description" placeholder="Dice up one carrot in large chunks..." required></textarea>
-                    </label>
+                <!-- Add additional step -->
+                <div class="flex items-center justify-end my-4">
+                    <a class="inline-flex items-center ml-2 text-green-600 hover:text-green-500" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                        </svg>
+                        Add
+                    </a>
                 </div>
             </fieldset>
         </div>
@@ -112,7 +110,7 @@
 
             <button
                 class="inline-flex items-center px-4 py-2 bg-green-800 text-green-50 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 ml-4">
-                Create
+                Add Recipe
             </button>
         </div>
 
--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="max-w-6xl mx-auto mt-6 px-6 py-4 sm:px-6 lg:px-8 bg-white shadow-md overflow-hidden sm:rounded-lg">
         <h1 class="text-2xl text-gray-800 mb-2"> {{ __('Add new recipe') }}</h1>
-        <p class="mt-2 text-gray-600 mb-8">{{ __('Create a new recipe by easily adding ingredients from our database, separate it out in portions and steps and if desired, add a beautiful cover photo.') }}</p>
+        <p class="mt-2 text-gray-600 mb-8">{{ __('Create a new recipe by easily adding ingredients from our database, separate it out in portions and steps.') }}</p>
 
         <!-- Handed down to VueJS -->
         <div id="app">
