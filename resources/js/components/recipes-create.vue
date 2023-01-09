2e5d13cf666f9638e61874916092587870a0d117
Mitchel Koster <mitchelkoster@gmail.com>
2023-01-09 14:17:03 +0100

    Multiple steps can now be added

--- resources/js/components/recipes-create.vue
+++ resources/js/components/recipes-create.vue
@@ -72,7 +72,7 @@
                         </select>
                     </div>
 
-                <div v-for="i in ingredientCount">
+                <div v-for="_ in ingredientCount">
                     <div class="my-4 flex flex-col md:flex-row justify-center items-stretch">
                         <!-- Ingredient name -->
                         <input
@@ -130,28 +130,34 @@
             <fieldset class="border px-4 rounded">
                 <legend class="text-2xl text-green-600 px-8">Steps</legend>
 
-                <!-- Step title -->
-                <div class="mt-4">
-                    <input id="step-title"
-                           class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
-                           type="text" name="title" placeholder="Vegetable prep (optional)" required
-                           autofocus/>
-                </div>
+                <div v-for="_ in stepCount">
+                    <!-- Step title -->
+                    <div class="mt-4">
+                        <input id="step-title"
+                            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
+                            type="text" name="title" placeholder="Vegetable prep (optional)"
+                            autofocus/>
+                    </div>
 
-                <!-- Step description -->
-                <div class="mt-4 mb-8">
-                    <textarea id="step-description" rows="5"
-                          class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
-                          type="email" name="description" placeholder="Dice up one carrot in large chunks, quarter a pepper, etc." required></textarea>
+                    <!-- Step description -->
+                    <div class="mt-4 mb-8">
+                        <textarea id="step-description" rows="5"
+                            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"
+                            type="email" name="description" placeholder="Dice up one carrot in large chunks, quarter a pepper, etc." required></textarea>
+                    </div>
                 </div>
 
                 <!-- Add additional step -->
-                <div class="flex items-center justify-end my-4">
-                    <a class="inline-flex items-center ml-2 text-green-600 hover:text-green-500" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                        </svg>
-                        Add
+                <div class="flex justify-end items-center my-4">
+                    <a class="inline-flex items-center text-green-600 hover:text-green-500" href="#" v-on:click.prevent="addStep">
+                        <div class="flex items-center justify-end my-4">
+                            <a class="inline-flex items-center ml-2 text-green-600 hover:text-green-500" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                                </svg>
+                                Add
+                            </a>
+                        </div>
                     </a>
                 </div>
             </fieldset>
@@ -178,6 +184,7 @@ export default {
     data() {
         return {
             ingredientCount: 1,
+            stepCount: 1,
             selectedSystem: 'Metric',
             ingredients: {},
             measurements: {},
@@ -202,6 +209,13 @@ export default {
         removeIngredient() {
             if (this.ingredientCount > 1)
                 this.ingredientCount -= 1
+        },
+        addStep() {
+            this.stepCount += 1
+        },
+        removeStep() {
+            if (this.stepCount > 1)
+                this.stepCount -= 1
         },
         getIngredients() {
             let url = `${window.location.origin}/api/ingredients`;
