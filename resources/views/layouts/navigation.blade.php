1a6268a22fbf84d4c29dfa513609721b4af036c5
Mitchel Koster <mitchelkoster@gmail.com>
2023-10-05 23:32:24 +0200

    Added logout form

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,12 @@
         @auth
             <a href="{{ url("/recipes/create") }}" class="p-3"><x-button>{{ __('Add Recipe') }}</x-button></a>
             <a href="{{ route('dashboard') }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">{{ __('Dashboard') }}</a>
-            <a href="{{ route('logout') }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">{{ __('Logout') }}</a>
+
+            {{-- Logout form --}}
+            <form method="POST" action="{{ route('logout') }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">
+                @csrf
+                <button type="submit">{{ __('Logout') }}</button>
+              </form>
         @else
             <a href="{{ route('login') }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">{{ __('Login') }}</a>
             <a href="{{ route('register') }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">{{ __('Register') }}</a>
