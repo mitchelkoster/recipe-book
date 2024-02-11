794b546ac39baa86dd3150d5a0ca05fdd5e1aece
Mitchel Koster <mitchelkoster@gmail.com>
2024-02-11 11:00:26 +0100

    Introduced a recipe URL

--- resources/js/components/recipes-create.vue
+++ resources/js/components/recipes-create.vue
@@ -160,6 +160,7 @@ export default {
             let baseUrl = window.location.origin;
             let urlPathParts = window.location.pathname.split('/');
 
+            // This should be abstracted to a general function
             // NOTE: This assumes you are hosting on a sub URL. E.g. "https://example.com/recipe-book/"
             if (urlPathParts.length > 3) {
                 urlPathParts.splice(2, 2);
