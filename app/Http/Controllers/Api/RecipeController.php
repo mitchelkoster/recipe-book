cb9f816e8c86841febd3f451808a39c7c605318d
Mitchel Koster <mitchelkoster@gmail.com>
2024-02-18 11:02:33 +0100

    Recipe tags can now be modified

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -80,33 +80,44 @@ class RecipeController extends Controller
             abort(400);
         }
 
-        // Update recipe
-        $foundRecipe->title = $request->input('title');
-        $foundRecipe->slug = Str::slug($request->input('title'), '-');
-        $foundRecipe->description = $request->input('description');
-        $foundRecipe->ingredients = $request->input('ingredients');
-        $foundRecipe->cover = $request->input('cover');
-        $foundRecipe->portions = $request->input('portions');
-        $foundRecipe->save();
+        // Start a database transaction
+        DB::transaction(function () use ($request, $foundRecipe, $recipe) {
+            // Update recipe
+            $foundRecipe->title = $request->input('title');
+            $foundRecipe->slug = Str::slug($request->input('title'), '-');
+            $foundRecipe->description = $request->input('description');
+            $foundRecipe->ingredients = $request->input('ingredients');
+            $foundRecipe->cover = $request->input('cover');
+            $foundRecipe->portions = $request->input('portions');
+            $foundRecipe->save();
 
-        // Update all steps in the recipe
-        $currentStepIds = [];
-        foreach ($request->input('steps') as $step) {
-            $tmp = Step::updateOrCreate(
-                ['id' => $step['id'], 'recipe_id' => $recipe->id],
-                ['description' => $step['title'], 'instructions' => $step['description'], 'recipe_id' => $recipe->id]
-            );
+            // Update all steps in the recipe
+            $currentStepIds = [];
+            foreach ($request->input('steps') as $step) {
+                $tmp = Step::updateOrCreate(
+                    ['id' => $step['id'], 'recipe_id' => $recipe->id],
+                    ['description' => $step['title'], 'instructions' => $step['description'], 'recipe_id' => $recipe->id]
+                );
 
-            $currentStepIds[] = $tmp['id'];
-        }
+                $currentStepIds[] = $tmp['id'];
+            }
 
-        // See which ID's no longer exist compared to the original so the steps can be removed
-        $stepsToRemove = $foundRecipe->steps->filter(function ($step) use ($currentStepIds) {
-            return !in_array($step['id'], $currentStepIds);
-        })->pluck('id');
+            // See which ID's no longer exist compared to the original so the steps can be removed
+            $stepsToRemove = $foundRecipe->steps->filter(function ($step) use ($currentStepIds) {
+                return !in_array($step['id'], $currentStepIds);
+            })->pluck('id');
 
-        // Remove steps
-        Step::destroy($stepsToRemove);
+            // Remove steps
+            Step::destroy($stepsToRemove);
+
+            // Update tags in the recipe
+            $tagIds = [];
+            foreach ($request->input('tags')as $tagName) {
+                $tag = Tag::firstOrCreate(['name' => $tagName]);
+                $tagIds[] = $tag->id;
+            }
+            $recipe->tags()->sync($tagIds);
+        });
 
         return response(NULL, 200);
     }
