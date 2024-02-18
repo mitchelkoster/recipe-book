f000da905d42906269fbef081b28925845063455
Mitchel Koster <mitchelkoster@gmail.com>
2024-02-18 12:18:57 +0100

    Recipe tags are now linked to filter

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -4,7 +4,7 @@
     <ul class="flex flex justify-start">
         @foreach ($tags as $tag)
         <li class="py-0.5 px-2 mx-1 bg-green-200 text-green-800 rounded">
-            <a href="#">{{ $tag->name }}</a>
+            <a href="{{ url('/tags') .'/' . $tag->name }}">{{ $tag->name }}</a>
         </li>
         @endforeach
     </ul>
