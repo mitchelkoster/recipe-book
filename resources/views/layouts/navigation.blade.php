ff45e2660149f5377031c0234f028edda4b0aec0
Mitchel Koster <mitchelkoster@gmail.com>
2024-11-15 00:53:37 +0100

    Changed collapse point to medium

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,7 +30,7 @@
     </div>
 
     <!-- Hamburger icon (collapsable menu) -->
-    <button id="menu-toggle" class="p-3 ml-auto sm:hidden text-green-600 hover:text-green-400">
+    <button id="menu-toggle" class="p-3 ml-auto md:hidden text-green-600 hover:text-green-400">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
