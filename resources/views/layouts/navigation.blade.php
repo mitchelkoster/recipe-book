060e3788dcf33da3d1fefb849728291b9673dabb
Mitchel Koster <mitchelkoster@gmail.com>
2024-11-15 01:49:08 +0100

    changed menu port to small

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
 <!-- Navigation links -->
 <nav class="flex w-full justify-between text-lg">
     <!-- Main Menu -->
-    <div class="w-full bg-white z-50 hidden md:flex md:items-center md:justify-between relative" id="navbar">
+    <div class="w-full bg-white z-50 hidden sm:flex md:items-center md:justify-between relative" id="navbar">
         <div class="flex">
             <a href="{{ url("/") }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">{{ __('Latest') }}</a>
             <a href="{{ url("/recipes") }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">{{ __('All') }}</a>
@@ -30,7 +30,7 @@
     </div>
 
     <!-- Hamburger icon (collapsable menu) -->
-    <button id="hamburger-menu-button" class="p-3 ml-auto md:hidden text-green-600 hover:text-green-400">
+    <button id="hamburger-menu-button" class="p-3 ml-auto sm:hidden text-green-600 hover:text-green-400">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
@@ -39,7 +39,7 @@
 
 {{-- TODO: Seperate mobile menu --}}
 <!-- Mobile Menu (hidden by default) -->
-<div id="mobile-menu" class="w-full hidden bg-white bg-opacity-90 md:hidden absolute left-0 top-[100%] z-40 h-screen">
+<div id="mobile-menu" class="w-full hidden bg-white bg-opacity-90 sm:hidden absolute left-0 top-[100%] z-40 h-screen">
     <div class="flex flex-col items-center py-4">
         <div class="flex flex-col items-center py-4 h-full">
             <a href="{{ url("/") }}" class="mt-1 block p-3 text-green-600 font-semibold rounded hover:text-green-400 sm:mt-0 sm:ml-1">{{ __('Latest') }}</a>
