3c3ad2c0015cca5816c865a2dbd4cee4d4f3caa3
Mitchel Koster <2762686+mitchelkoster@users.noreply.github.com>
2025-06-01 16:46:30 +0200

    Add wrapping to tags

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -1,9 +1,9 @@
 @props(['tags'])
 
 <div class="text-center mt-4">
-    <ul class="flex flex justify-start">
+    <ul class="flex flex-wrap justify-start">
         @foreach ($tags as $tag)
-        <li class="py-0.5 px-2 mx-1 bg-green-200 text-green-800 rounded">
+        <li class="py-0.5 px-2 mx-1 mt-2 bg-green-200 text-green-800 rounded">
             <a href="{{ url('/tags', $tag->name) }}">{{ $tag->name }}</a>
         </li>
         @endforeach
