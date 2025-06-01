5f1eff7fb74882b594485f0fca9342ef7075db61
Mitchel Koster <2762686+mitchelkoster@users.noreply.github.com>
2025-06-01 17:02:20 +0200

    Tags are now converted to lower case

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Tag;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class TagFactory extends Factory
 {
@@ -22,7 +23,7 @@ class TagFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->unique()->word()
+            'name' => Str::of($this->faker->unique()->word())->lower()
         ];
     }
 }
