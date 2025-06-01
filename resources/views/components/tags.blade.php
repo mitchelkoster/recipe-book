09a4291e4f20dacb91f3de085c407cf8f58163bf
Mitchel Koster <2762686+mitchelkoster@users.noreply.github.com>
2025-06-01 16:55:08 +0200

    Make tag position dynamic

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -1,7 +1,7 @@
-@props(['tags'])
+@props(['tags', 'position' => 'start'])
 
 <div class="text-center mt-4">
-    <ul class="flex flex-wrap justify-start">
+    <ul class="flex flex-wrap justify-{{ $position }}">
         @foreach ($tags as $tag)
         <li class="py-0.5 px-2 mx-1 mt-2 bg-green-200 text-green-800 rounded">
             <a href="{{ url('/tags', $tag->name) }}">{{ $tag->name }}</a>
