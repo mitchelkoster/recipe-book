038c8096b4be9c8f5a2b7837f0d727609c203bbc
Mitchel Koster <2762686+mitchelkoster@users.noreply.github.com>
2025-09-13 22:04:15 +0200

    Update to "show favorite" on the recipes.show
    Also show even if you did not create the recipe

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -33,6 +33,18 @@
                     </button>
                 </form>
             </div>
+            @elseif (auth()->check())
+                {{-- Favorite recipe --}}
+                <div class="flex flex-row justify-end mt-4 px-8 w-full">
+                    <form method="POST" action="{{ route('recipes.favorite', $recipe) }}">
+                        @csrf
+                        <button type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="mt-1 w-8 h-8 text-red-400 hover:text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                            </svg>
+                        </button>
+                    </form>
+                </div>
             @endif
 
             <!-- Title and description -->
